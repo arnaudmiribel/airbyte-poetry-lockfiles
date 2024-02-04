4a7fb80d7855aa17679c99536a3cfaae89bfcbe0
Arnaud <arnaudmiribel@gmail.com>
2024-02-04 20:08:59 +0100

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,45 @@
+import streamlit as st
+from ghapi.all import GhApi
+from matplotlib import pyplot as plt
+
+QUERY = "repo:airbytehq/airbyte airbyte-integrations/connectors in:path poetry.lock"
+
+@st.cache_resource
+def get_api():
+    return GhApi(
+        token=st.secrets.ACCESS_TOKEN,
+    )
+
+@st.cache_data(ttl=24 * 60 * 60)
+def get_data(_api: GhApi, query: str):
+    results = _api.search.code(q=query)
+    return results
+
+api = get_api()
+
+st.title("🐙")
+st.header("Poetry Lock Files in Airbyte Connectors")
+
+data = get_data(
+    _api=api,
+    query=QUERY,
+)
+
+def format_path(path: str) -> str:
+    return path.removeprefix("airbyte-integrations/connectors/")
+
+if data:
+    st.subheader("Summary")
+    total_count = data.total_count
+    st.metric("Num. connectors using poetry.lock", f"{total_count}  🎉")
+
+    st.subheader("Connectors")
+    for item in dict(data)["items"]:
+        st.link_button(format_path(item.path), item.html_url, use_container_width=True)
+
+    st.divider()
+  
+    with st.expander("Raw data"):
+        st.json(data)
+else:
+    st.error("No data to display")
