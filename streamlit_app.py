272ddc3a1e390becd71cd7d5d3771059c5b63b9f
Arnaud <arnaudmiribel@gmail.com>
2024-02-04 21:04:53 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,13 @@ from ghapi.all import GhApi
 from pathlib import Path
 import pandas as pd
 
-# Constants
-QUERY = "repo:airbytehq/airbyte airbyte-integrations/connectors in:path poetry.lock"
+QUERY_POETRY = (
+    "repo:airbytehq/airbyte airbyte-integrations/connectors in:path poetry.lock"
+)
+QUERY_REQUIREMENTS = (
+    "repo:airbytehq/airbyte airbyte-integrations/connectors in:path requirements.txt"
+)
+
 
 @st.cache_resource
 def get_api():
@@ -12,10 +17,12 @@ def get_api():
         token=st.secrets.ACCESS_TOKEN,
     )
 
+
 @st.cache_data(ttl=24 * 60 * 60)
 def get_data(_api: GhApi, query: str):
     results = _api.search.code(q=query)
     return results
+
 
 @st.cache_data
 def get_connectors() -> [pd.DataFrame, pd.DataFrame]:
@@ -28,7 +35,9 @@ def get_connectors() -> [pd.DataFrame, pd.DataFrame]:
 
     # Restrict to useful columns
     sources_df = sources_df[["Connector Name", "Support Level", "OSS", "Cloud"]]
-    destinations_df = destinations_df[["Connector Name", "Support Level", "OSS", "Cloud"]]
+    destinations_df = destinations_df[
+        ["Connector Name", "Support Level", "OSS", "Cloud"]
+    ]
 
     # Turn emojis into boolean (checkbox) column
     for column in ("OSS", "Cloud"):
@@ -37,18 +46,22 @@ def get_connectors() -> [pd.DataFrame, pd.DataFrame]:
 
     return sources_df, destinations_df
 
+
 def format_path(path: str) -> str:
-    return (
-        path
-        .removeprefix("airbyte-integrations/connectors/destination-")
-        .removesuffix("/poetry.lock")
-    )
+    """ Extract connector name from path. """
+    return path.removeprefix("airbyte-integrations/connectors/").split("/")[0]
+
 
 api = get_api()
 
-data = get_data(
+poetry_data = get_data(
     _api=api,
-    query=QUERY,
+    query=QUERY_POETRY,
+)
+
+requirements_data = get_data(
+    _api=api,
+    query=QUERY_REQUIREMENTS,
 )
 
 sources_df, destinations_df = get_connectors()
@@ -56,22 +69,38 @@ sources_df, destinations_df = get_connectors()
 st.title("🐙")
 st.header("Poetry Lock Files in Airbyte Connectors")
 
-""" This app helps monitoring our progress as we migrate 
-all Airbyte connectors to Poetry. To do so, we lookup the 
-amount of `poetry.lock` files under the `airbyte-integrations/connectors` 
-directory of the main `airbytehq/airbyte` repository. 
+""" This app helps monitoring our progress as we migrate
+all Airbyte connectors to Poetry. To do so, we lookup the
+amount of `poetry.lock` files under the `airbyte-integrations/connectors`
+directory of the main `airbytehq/airbyte` repository.
 
 Reach out to @alafanechere for feedback or help!
 """
 
-if data:
+if poetry_data:
     st.subheader("Summary")
-    total_count = data.total_count
-    st.metric("Num. connectors using poetry.lock", f"{total_count}  🎉")
+
+    left, middle, right = st.columns((2, 2, 1))
+    left.metric("Num. connectors using poetry.lock", f"{poetry_data.total_count}  🎉")
+    middle.metric(
+        "Num. connectors using requirements.txt", f"{requirements_data.total_count}"
+    )
+    right.metric(
+        "Percent migrated",
+        f"{poetry_data.total_count / (poetry_data.total_count + requirements_data.total_count):.0%}",
+    )
 
     st.subheader("Migrated Connectors")
-    for item in dict(data)["items"]:
-        st.link_button(format_path(item.path), item.html_url, use_container_width=True)
+    for item in dict(poetry_data)["items"]:
+        if item.path.startswith(
+            (
+                "airbyte-integrations/connectors/source-",
+                "airbyte-integrations/connectors/destination-",
+            )
+        ):
+            st.link_button(
+                format_path(item.path), item.html_url, use_container_width=True
+            )
 
     st.subheader("All Connectors")
     st.write("Sources")
@@ -79,11 +108,5 @@ if data:
     st.write("Destinations")
     st.dataframe(destinations_df, use_container_width=True, hide_index=True)
 
-    st.divider()
-    with st.expander("Debug"):
-        st.caption("Query")
-        st.code(QUERY)
-        st.caption("Output")
-        st.json(data)
 else:
     st.error("No data to display")
